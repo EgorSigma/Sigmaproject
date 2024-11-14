b3063ae49aa50e5d5ed3672b35b68611787ca4b1
EgorSigma <egoryudenkov2010@gmail.com>
2024-11-14 22:51:18 +0100

    sigma project all 32

--- main.js
+++ main.js
@@ -1,36 +1,77 @@
-let menu=document.querySelector(".fixed-menu");
+let menu = document.querySelector(".fixed-menu"); 
+menu.addEventListener("click", function (event) { 
+  let clickTarget = event.target; 
+  let activeBtn = document.querySelector('.active'); 
+ 
+  if (clickTarget.classList.contains('nav-link')) { 
+    clickTarget.classList.add('active'); 
+    activeBtn.classList.remove('active'); 
+  } 
+}); 
+let body = document.querySelector("body"); 
+let mobileMenu = document.querySelector(".mobile-menu"); 
+if (!body.classList.contains("off-scroll") && 
+  !mobileMenu.classList.contains("hide")) { 
+  body.classList.add("off-scroll") 
+} 
+else { 
+  body.classList.remove("off-scroll") 
+} 
+let classlink = ".main-link"; 
+window.onscroll = function () { 
+  let h = document.documentElement.clientHeight; 
+  if (window.scrollY >= h * 4) { 
+    classlink = ".comments-link"; 
+  } 
+  else if (window.scrollY >= h * 3) { 
+    classlink = ".works-link"; 
+  } 
+  else if (window.scrollY >= h * 2) { 
+    classLink = '.skills-link'; 
+  } 
+  else if (window.scrollY >= h) { 
+    classLink = '.about-link'; 
+  } 
+  else { 
+    classLink = '.main-link'; 
+  } 
+  let activeBtn = document.querySelector('.active'); 
+  let newActiveBtn = document.querySelector(classLink); 
+  if (!newActiveBtn.classList.contains('active')) { 
+    newActiveBtn.classList.add('active'); 
+    activeBtn.classList.remove('active') 
+  } 
+}; 
+document.querySelector(".mobile-button").addEventListener("click", 
+  function (event) { 
+    document.querySelector(".mobile-menu").classList.toggle("hide"); 
+  }); 
+ 
+menu.addEventListener('click', function (event) { 
+  let clickTarget = event.target; 
+  let activeBtn = document.querySelector('.active'); 
+  if (clickTarget.classList.contains('nav-link')) { 
+    clickTarget.classList.add('active'); 
+    activeBtn.classList.remove('active'); 
+ 
+    let mobile = document.querySelector('.mobile-menu'); 
+    if (!mobile.classList.contains('hide')) { 
+      mobile.classList.add('hide'); 
+    } 
+ 
+  } 
+}); 
+ 
+document.querySelector(".mobile-button").addEventListener("click",
+  function (event) {
+    document.querySelectorAll(".mobile-btn").forEach(function (item) {
+      item.classList.toggle('btn-hide');
+    })
+    if (document.querySelector(".mobile-menu").contains('hide')) {
+      document.querySelector(".mobile-menu").remove('hide')
+    } else {
+      document.querySelector(".mobile-menu").add('hide')
+    }
+  });
 
-menu.addEventListener("click",function(event){
-    let clickTarget = event.target;
-    let activeBtn = document.querySelector('.active');
-    
-    if (clickTarget.classList.contains('nav-link')){
-        clickTarget.classList.add('active');
-        activeBtn.classList.remove('active');
-    }
- });
- let classlink=".main-link";
- window.onscroll=function(){
-    let h=document.documentElement.clientHeight;
-    if(window.scrollY>=h*4){
-        classlink=".comments-link";
-    }
-    else if(window.scrollY>=h*3){
-        classlink=".works-link";
-    }
-  else if (window.scrollY >= h*2 ){
-    classLink = '.skills-link';
-  }
-  else if (window.scrollY >= h){
-    classLink = '.about-link';
-  }
-  else{
-    classLink = '.main-link';
-  }
-  let activeBtn = document.querySelector('.active');
-  let newActiveBtn = document.querySelector(classLink);
-  if (!newActiveBtn.classList.contains('active')){
-    newActiveBtn.classList.add('active');
-    activeBtn.classList.remove('active')
-  }
-};
+
